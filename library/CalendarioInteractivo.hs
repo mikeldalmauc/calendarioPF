fe2ac49622cedca03828fdc1622b882723590cf7
Julen Fernandino Urroz <fernandino.julen@gmail.com>
2018-12-12 11:13:10 +0100

    Añadido calendario interactivo

---
+++ library/CalendarioInteractivo.hs
@@ -0,0 +1,25 @@
+module CalendarioInteractivo where
+
+    import Calendarios (printCalendario)
+    
+    main :: IO ()
+    main = do
+        putStr "CALENDARIO"
+        putStr "\n Give me a year (a>0) \n a = "
+        a <- leerEnt
+        putStr "\n Give me the number of cuolumns \n c = "
+        c <- leerEnt
+        putStr "\n Give me the language (\"es\", ... ) \n l = "
+        l <- leerString
+        printCalendario c a l
+    
+    leerEnt :: IO Int
+    leerEnt = do 
+        e <- getLine
+        return (read e)
+    
+    leerString :: IO String
+    leerString = do 
+        e <- getLine
+        return (read e)
+    
